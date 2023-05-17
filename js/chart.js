3ef9b8269e9dc02381065fa945a9f0d9374af105
zhiren2202 <132483183+zhiren2202@users.noreply.github.com>
2023-05-17 16:04:34 +0900

    Add files via upload

--- js/chart.js
+++ js/chart.js
@@ -11,6 +11,7 @@ google.charts.setOnLoadCallback(drawChart2_3);
 google.charts.setOnLoadCallback(drawChart2_4);
 google.charts.setOnLoadCallback(drawChart2_5);
 google.charts.setOnLoadCallback(drawChart2_6);
+google.charts.setOnLoadCallback(drawChart2_7);
 
 google.charts.setOnLoadCallback(drawChart3_1);
 google.charts.setOnLoadCallback(drawChart3_2);
@@ -128,7 +129,7 @@ function drawChart1_3() {
     var options = {
         width: '100%',
         height: 400,
-        title: 'Column Chart',
+        title: 'Column Chart //말풍선 에러 수정',
         colors: ['#e0440e', '#e6693e', '#ec8f6e', '#f3b49f', '#f6c7b6'],
         legend: { position: "none" },
         chartArea:{
@@ -141,11 +142,9 @@ function drawChart1_3() {
 
     chart.draw(view, options);
 
-}
-
-// columnchart_ani
+}// columnchart_ani
 function drawChart1_4() {
-    
+
     var data = google.visualization.arrayToDataTable([
         ['Element', 'Density', { role: 'style' }],
         ['Copper', 8.94, '#b87333'],
@@ -160,7 +159,7 @@ function drawChart1_4() {
             calc: "stringify",
             sourceColumn: 1,
             type: "string",
-            role: "annotation"
+            role: "annotation",
         },
         2]);
 
@@ -179,11 +178,8 @@ function drawChart1_4() {
     var chart = new google.visualization.ColumnChart(document.getElementById('columnchart_ani'));
 
     chart.draw(view, options);
-
+    
     }
-
-
-
 
 
 // 2. Piechart
@@ -202,7 +198,7 @@ function drawChart2_1() {
     var options = {
         width: '100%',
         height: 300,
-        title: 'Pie Chart',
+        title: 'Pie Chart //원형 크기 수정',
         colors: ['#e0440e', '#e6693e', '#ec8f6e', '#f3b49f', '#f6c7b6'],
         backgroundColor: 'none',
         chartArea:{
@@ -234,8 +230,7 @@ function drawChart2_2() {
         height: 300,
         title: 'Doughnut Chart',
         colors: ['#e0440e', '#e6693e', '#ec8f6e', '#f3b49f', '#f6c7b6'],
-        // backgroundColor: 'none',
-        backgroundColor: 'gray',
+        backgroundColor: 'none',
         pieHole: 0.4,
         chartArea:{
             width:'80%',
@@ -325,8 +320,8 @@ function drawChart2_5() {
 
     var options = {
         width: '100%',
-        height: 300,
-        title: 'Pie Chart - Slices',
+        height: 500,
+        title: 'Pie Chart - Slices //슬라이스 여러개 샘플 추가',
         colors: ['#e0440e', '#e6693e', '#ec8f6e', '#f3b49f', '#f6c7b6'],
         backgroundColor: 'none',
         slices: { 
@@ -334,7 +329,7 @@ function drawChart2_5() {
             3: { offset: 0.2 }, },
         chartArea:{
             width:'90%',
-            height:'70%'
+            height:'50%'
         }
     };
 
@@ -357,19 +352,62 @@ function drawChart2_6() {
 
     var options = {
         width: '100%',
-        height: 300,
+        height: 500,
         title: 'Pie Chart - Color',
         colors: ['#B9EDDD', '#87CBB9', '#569DAA', '#577D86', '#0A4D68'],
         backgroundColor: 'none',
         chartArea:{
             width:'90%',
-            height:'70%'
+            height:'50%'
         }
     };
 
     var chart = new google.visualization.PieChart(document.getElementById('piechart_color'));
 
     chart.draw(data, options);
+
+}
+
+// piechart_ani
+function drawChart2_7() {
+
+    value = 0;
+    maxValue = 40;
+    increment = 1;
+
+    var data = google.visualization.arrayToDataTable([
+        ['Task', 'Hours per Day'],
+        ['data1', value],
+        ['data2', 100-value],
+    ]);
+    var options = {
+        title: 'Pie Chart - Animation',
+        colors: ['#e6693e', '#f3b49f'],
+        animation: {
+                duration: 1000,
+                easing: 'in',
+        },
+        width: '100%',
+        height: 500,
+        chartArea:{
+            width:'90%',
+            height:'70%'
+        }
+    };
+    var chart = new google.visualization.PieChart(document.getElementById('piechart_ani'));
+
+    chart.draw(data, options);
+
+    var interval = setInterval(function(){
+        if(value>maxValue-increment) {clearInterval(interval)}
+
+        chart.draw(data, options);
+
+        value+=increment;
+        data.setValue(0,1,value)
+        data.setValue(1,1,100-value)
+
+    },20)
 
 }
 
@@ -393,6 +431,10 @@ function drawChart3_1() {
         colors: ['#e0440e', '#e6693e', '#ec8f6e', '#f3b49f', '#f6c7b6'],
         backgroundColor: 'none',
         legend: 'labeled',
+        chartArea:{
+            width:'90%',
+            height:'70%'
+        }
     };
 
     var chart = new google.visualization.PieChart(document.getElementById('piechart_legend_labeled'));
@@ -416,10 +458,18 @@ function drawChart3_2() {
     var options = {
         width: '100%',
         height: 400,
-        title: 'Pie Chart - 범례위치 (Top)',
+        title: 'Pie Chart - 범례위치 (Top) // 페이징 버튼 안나오게 설정',
         colors: ['#e0440e', '#e6693e', '#ec8f6e', '#f3b49f', '#f6c7b6'],
         backgroundColor: 'none',
-        legend: 'top',
+        legend: {
+            position: 'top',
+            alignment: 'center',
+            maxLines: 5
+        },
+        chartArea:{
+            width:'90%',
+            height:'70%'
+        }
     };
 
     var chart = new google.visualization.PieChart(document.getElementById('piechart_legend_position_top'));
@@ -446,7 +496,14 @@ function drawChart3_3() {
         title: 'Pie Chart - 범례위치 (Bottom)',
         colors: ['#e0440e', '#e6693e', '#ec8f6e', '#f3b49f', '#f6c7b6'],
         backgroundColor: 'none',
-        legend: 'bottom',
+        legend: {
+            position: 'bottom',
+            alignment: 'center',
+        },
+        chartArea:{
+            width:'90%',
+            height:'70%'
+        }
     };
 
     var chart = new google.visualization.PieChart(document.getElementById('piechart_legend_position_bottom'));
@@ -474,6 +531,10 @@ function drawChart3_4() {
         colors: ['#e0440e', '#e6693e', '#ec8f6e', '#f3b49f', '#f6c7b6'],
         backgroundColor: 'none',
         legend: 'right',
+        chartArea:{
+            width:'90%',
+            height:'70%'
+        }
     };
 
     var chart = new google.visualization.PieChart(document.getElementById('piechart_legend_position_right'));
@@ -501,6 +562,10 @@ function drawChart3_5() {
         colors: ['#e0440e', '#e6693e', '#ec8f6e', '#f3b49f', '#f6c7b6'],
         backgroundColor: 'none',
         legend: 'left',
+        chartArea:{
+            width:'90%',
+            height:'70%'
+        }
     };
 
     var chart = new google.visualization.PieChart(document.getElementById('piechart_legend_position_left'));
@@ -574,7 +639,7 @@ function drawChart4_3() {
     var options = {
         width: '100%',
         height: 'auto',
-        title: 'Line Chart',
+        title: 'Line Chart // 직선 그래프 샘플 추가',
         legend: { position: 'bottom' }
     };
 
@@ -596,7 +661,7 @@ function drawChart4_4() {
     var options = {
         width: '100%',
         height: 'auto',
-        title: 'Area Chart',
+        title: 'Area Chart // 영역 그래프 샘플 추가',
         hAxis: {title: 'Year',  titleTextStyle: {color: '#333'}},
         vAxis: {minValue: 0},
         legend: { position: 'bottom' },
